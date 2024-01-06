3a997ba871c97d3b2dd253f14bafce3ca70bc557
Emersson04 <emerssonramirez11@gmail.com>
2024-01-06 15:14:42 -0500

    estilos a botones de tabla de usuarios

--- resources/views/crud/LeerUsuarios.blade.php
+++ resources/views/crud/LeerUsuarios.blade.php
@@ -27,7 +27,7 @@
                     <td class="px-6 py-3 bg-gray-50 dark:bg-gray-800">{{$usuario -> cedula_user}}</td>
                     <td class="px-6 py-3 bg-gray-50 dark:bg-gray-800">{{$usuario -> num_telefono}}</td>
                     <td class="px-6 py-3 bg-gray-50 dark:bg-gray-800">{{$usuario -> direccion_user}}</td>
-                    <td>  <a href="{{route('editar.index', $usuario->id)}}"
+                    <td class="flex">  <a href="{{route('editar.index', $usuario->id)}}"
                              class="text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-yellow-900"
                         >
                             EDITAR</a>
@@ -36,7 +36,7 @@
                             @csrf
 
                             {{method_field('DELETE')}}
-                            <input type="submit" value="Delete" class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                            <input type="submit" value="Delete" class="text-white cursor-pointer bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
                         </form>
                     </td>
                 </tr>
